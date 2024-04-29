801738d0564f72ce8983f35ec63347d66d759ef7
juanmarchant <jua.marchant@duocuc.cl>
2024-04-29 17:06:10 -0400

    nothing selected view

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,17 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
 
-            <Typography >Nostrud nulla commodo sint ut tempor veniam aute qui occaecat incididunt adipisicing sunt consequat irure. Culpa eu anim aliquip velit ut. Nulla exercitation et reprehenderit proident adipisicing non est occaecat.</Typography>
+            {/* <Typography >Nostrud nulla commodo sint ut tempor veniam aute qui occaecat incididunt adipisicing sunt consequat irure. Culpa eu anim aliquip velit ut. Nulla exercitation et reprehenderit proident adipisicing non est occaecat.</Typography> */}
 
             {/* NothinSelected */}
+            <NothingSelectedView />
             {/* NoteView */}
         </JournalLayout>
     )
--- src/journal/views/NothingSelectedView.jsx
+++ src/journal/views/NothingSelectedView.jsx
@@ -1,9 +1,30 @@
+import { StarOutline, StartOutlined } from "@mui/icons-material"
+import { Grid, Typography } from "@mui/material"
 
 
 
 
 export const NothingSelectedView = () => {
     return (
-        <div>NothingSelectedView</div>
+        <Grid container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            sx={{ minHeight: 'calc(100vh - 100px)', backgroundColor: 'primary.main' }}
+        >
+            <Grid item
+                xs={12}
+            >
+                <StarOutline sx={{ fontSize: 100, color: 'white' }} />
+            </Grid>
+
+            <Grid item
+                xs={12}
+            >
+                <Typography color="white" variant="h5"> Selecciona o crea una entrada</Typography>
+            </Grid>
+        </Grid >
+
     )
 }
