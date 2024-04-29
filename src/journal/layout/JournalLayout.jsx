1b56a466fb9a4bd959eb074b502a32421be8f342
juanmarchant <jua.marchant@duocuc.cl>
2024-04-29 16:59:52 -0400

    sidebar added

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material"
-import { NavBar } from "../components";
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components";
 
 
 const drawerWidth = 240;
@@ -12,11 +12,13 @@ export const JournalLayout = ({ children }) => {
             <NavBar drawerWidth={drawerWidth} />
             {/* Sidebar */}
 
+            <SideBar drawerWidth={drawerWidth} />
+
             <Box
                 component='main'
                 sx={{ flexGrow: 1, p: 3 }}
             >
-                {/* Toolbar */}
+                <Toolbar />
                 {children}
             </Box>
         </Box>
---
+++ src/journal/views/NothingSelectedView.jsx
@@ -0,0 +1,9 @@
+
+
+
+
+export const NothingSelectedView = () => {
+    return (
+        <div>NothingSelectedView</div>
+    )
+}
